46d3ce14f054492a36bce4899d20432a39e5a1b7
Remi Gastaldi <remi.gastaldi@epitech.eu>
2019-05-23 20:28:30 -0400

    Fix docker start

--- src/index.ts
+++ src/index.ts
@@ -25,3 +25,5 @@ export default class YoutubePlayer {
       this.widget.html("<iframe id=\"ytplayer\" type=\"text/html\" width=\"640\" height=\"360\" src=\"http://www.youtube.com/embed/DaYx4XmWEoI?autoplay=1&controls=0&disablekb=1&modestbranding=1&rel=0&showinfo=0\" frameborder=\"0\"/>");
     }
 }
+
+new YoutubePlayer().start();
